05c04e7ecaa2294d37ddc05ffcae848ae571b64a
Eric Hodges <ehodges2004@gmail.com>
2016-03-17 09:53:21 -0700

    Added function to stocktwits display and added flex boxes for layout

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,13 +1,12 @@
 app.controller('mainController', function($scope, $http){
 
 	$http.get('https://api.stocktwits.com/api/2/streams/symbol/aapl.json').then(function(response){
-		$scope.data = response.data;
-		
+		$scope.data = response.data;		
 		$scope.messages = response.data.messages;
 
 
 	});
 
-	
+
 
 });
